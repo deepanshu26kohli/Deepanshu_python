899ea70be4afc887028a726449ace6adbffb3954
deepanshu26kohli <95180440+deepanshu26kohli@users.noreply.github.com>
2021-12-02 14:28:06 +0530

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,16 @@
+d={"dog","cat","elephant","tiger"}
+print(d)
+d.pop()
+print(d)
+d.discard("cow")
+print(d)
+w={"buffalo","prince","cat"}
+d.update(w)
+print(d)
+print(d.union(w))
+print(d)
+print(d.intersection(w))
+print(len(d))
+
+
+
